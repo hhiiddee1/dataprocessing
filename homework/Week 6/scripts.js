aa0e4bcc1ab63a64c30e7016079d208289bc3a79
hhiiddee1 <hidde.van.oijen@gmail.com>
2018-12-12 16:18:13 +0100

    Update scripts.js

--- homework/Week 6/scripts.js
+++ homework/Week 6/scripts.js
@@ -8,38 +8,78 @@ d3v5.json("data.json").then(function(datacsv) {
       for (const [keys] of Object.entries(datacsv)) {
         country = datacsv[keys];
         countries.push(country);
-        percentage = datacsv[keys]["2015"];
+        percentage = datacsv[keys]["year2015"];
         year2015.push(percentage);
       }
       console.log(countries);
       console.log(year2015);
       console.log(datacsv)
-      makeSvgs()
+
+      var w = 800;
+      var h = 400;
+
+      var svgLineChart = d3v5.select("body")
+                          .append("svg")
+                          .attr("id", "svgLineChart")
+                          .attr("width", w)
+                          .attr("height", h);
+
+      makeAxis(svgLineChart)
 
       makeMap(datacsv)
+
+      makeText(svgLineChart)
+
+      makeLineChart(svgLineChart, datacsv)
 });
 
+function makeAxis(svg) {
+  // makes scale for Y axis
+  var scaleY = d3v5.scaleLinear()
+                  .domain([0, 100])
+                      .range([200, 0]);
 
-function makeSvgs() {
-var w = 600;
-var h = 400;
+  var scaleX = d3v5.scaleLinear()
+                  .domain([0,50])
+                  .range([0, 400]);
+  //Create Y axis
+  svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(100,100 )")
+    .call(d3v5.axisLeft(scaleY));
 
-var svgWorldMap = d3.select("body")
-            .append("svg")
-            .attr("id", "svgWorldMap")
-            .attr("width", w)
-            .attr("height", h);
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(100, 300 )")
+    .call(d3v5.axisBottom(scaleX));
 
-var svgWorldMap = d3.select("body")
-                    .append("svg")
-                    .attr("id", "svgLineChart")
-                    .attr("width", w)
-                    .attr("height", h);
 }
 
-// function makeAxis(svg) {
-//
-// }
+function makeText(svg) {
+  svg.append("text")
+  .text("% Renewable energy of total energie per country over different years")
+  .attr("x", 20)
+  .attr("y", 80)
+  .attr("id", "title")
+  .attr("font-weight","bold")
+  .attr("font-size", "19px");
+
+  // makes text for Y axis
+  svg.append("text")
+    .text("Year")
+    .attr("x", 110)
+    .attr("y", 350)
+    .attr("font-weight","bold");
+
+  // makes text for X axis
+  svg.append("text")
+    .text("% renewable energie")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -280)
+    .attr("y", 50)
+    .attr("font-weight","bold");
+  }
+
 function makeMap(datacsv) {
     var map = new Datamap({
         element: document.getElementById('container'),
@@ -57,11 +97,29 @@ function makeMap(datacsv) {
           geographyConfig: {
   highlightBorderColor: '#bada55',
   popupTemplate: function(geography, data) {
-    console.log(datacsv)
-     return '<div class="hoverinfo">' + geography.properties.name + '% renewable energy: ' +  data.year2015 + ' '
+     return '<div class="hoverinfo">' + geography.properties.name + ': % renewable energy: ' +  data.year2015 + ' '
    },
    highlightBorderWidth: 3
  },
-data: datacsv
+data: datacsv,
+
+done: function(datamap) {
+       datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+           var state_id = geography.id;
+           console.log(state_id)
+       })
+     }
 });
+}
+
+function makeLineChart(svg, data){
+
+  var line = d3.line()
+      .x(function(d, i) { return xScale(i); })
+      .y(function(d) { return yScale(d); })
+
+  svg.append("path")
+      .datum(data["AUT"])
+      .attr("class", "line")
+      .attr("d", line);
 }
